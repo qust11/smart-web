7667f654236a384453818d44f1279b37e37e3471
qushutao <qushutao@ygemail.com>
2025-09-04 09:04:05 +0800

    feat(security): 添加 Web 安全配置
    - 在 SecurityConfig 类中添加 @EnableWebSecurity 注解
    - 更新类定义，使其支持 Web 安全配置

--- src/main/java/com/ym/smartweb/config/SecurityConfig.java
+++ src/main/java/com/ym/smartweb/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.ym.smartweb.config;
 
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
 /**
  * @author qushutao
  * @since 2025/9/1 19:11
  **/
+@EnableWebSecurity
 public class SecurityConfig  {
 }
