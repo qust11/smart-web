f7eb7aa8d36b57bec3b9cc6645ea0fc758e52bd6
qushutao <qushutao@ygemail.com>
2025-09-04 19:42:22 +0800

    feat(security): 添加跨域资源共享配置
    - 在 SecurityConfig 中添加 SecurityFilterChain Bean
    - 配置 HttpSecurity 以启用 CORS 默认设置
    - 更新 FilterConfig 文件，添加必要导入
    - 在 springsecurity源码学习.md 中添加 AbstractConfiguredSecurityBuilder 相关知识

--- src/main/java/com/ym/smartweb/config/SecurityConfig.java
+++ src/main/java/com/ym/smartweb/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.ym.smartweb.config;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * @author qushutao
@@ -10,4 +13,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  **/
 @EnableWebSecurity
 public class SecurityConfig  {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults());
+        return http.build();
+    }
 }
--- src/main/springsecurity源码学习.md
+++ src/main/springsecurity源码学习.md
@@ -6,3 +6,4 @@
 
 
 * SecurityConfigurer 配置类用于配置SecurityBuilder构建的对象
+* AbstractConfiguredSecurityBuilder(继承自 AbstractSecurityBuilder,内部持有SecurityConfigurer集合) 定义了个doBuild模板方法 ，分别有beforeInit init beforeConfig config performBuild 由子类实现
