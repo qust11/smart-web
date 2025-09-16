eab40baf1dc71b981f074a9943f49b65e06f2a63
qushutao <qushutao@ygemail.com>
2025-09-16 09:26:36 +0800

    refactor(auth): 直接注入密码编解码器

--- src/main/java/com/ym/smartweb/auth/handler/SuccessHandler.java
+++ src/main/java/com/ym/smartweb/auth/handler/SuccessHandler.java
@@ -2,7 +2,6 @@ package com.ym.smartweb.auth.handler;
 
 import com.ym.smartweb.pojo.CommonResponse;
 import com.ym.smartweb.util.ResponseUtil;
-import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
--- src/main/java/com/ym/smartweb/config/SecurityConfig.java
+++ src/main/java/com/ym/smartweb/config/SecurityConfig.java
@@ -7,13 +7,10 @@ import com.ym.smartweb.auth.handler.SuccessHandler;
 import com.ym.smartweb.encoder.CustomPasswordEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 /**
  * @author qushutao
@@ -37,13 +34,5 @@ public class SecurityConfig {
                 .authorizeHttpRequests(c -> c.anyRequest().authenticated())
                 .exceptionHandling(c -> c.authenticationEntryPoint(entryPoint));
         return http.build();
-    }
-    // 配置认证提供者使用自定义密码编码器
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider(UserDetailsService userDetailsService,
-                                                            CustomPasswordEncoder customPasswordEncoder) {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
-        authProvider.setPasswordEncoder(customPasswordEncoder);
-        return authProvider;
     }
 }
